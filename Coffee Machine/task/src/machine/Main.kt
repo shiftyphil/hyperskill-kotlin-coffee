f76307f02b50a96e99a193b0678484210b117bb7
Philip Higgins <phil@pjh.id.au>
2019-09-10 20:16:36 +1000

    Solve Stage 3

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -2,14 +2,34 @@ package machine
 
 import java.util.Scanner
 
+class CoffeeMachine(val waterSupply: Int, val milkSupply: Int, val beanSupply: Int) {
+    fun getPossibleCups(): Int {
+        return minOf(waterSupply / 200, milkSupply / 50, beanSupply / 15)
+    }
+}
+
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("Write how many cups of coffee you will need: ")
-    val numCups = scanner.nextInt()
+    println("Write how many ml of water the coffee machine has: ")
+    val waterSupply = scanner.nextInt()
+    println("Write how many ml of milk the coffee machine has: ")
+    val milkSupply = scanner.nextInt()
+    println("Write how many g of coffee beans the coffee machine has: ")
+    val beanSupply = scanner.nextInt()
 
-    println("For $numCups of coffee you will need:")
-    println("${numCups * 200} ml of water")
-    println("${numCups * 50} ml of milk")
-    println("${numCups * 15} g of coffee beans")
+    val machine = CoffeeMachine(waterSupply, milkSupply, beanSupply)
+
+    println("Write how many cups of coffee you will need: ")
+    val requiredCups = scanner.nextInt()
+    val possibleCups = machine.getPossibleCups()
+
+    val difference = possibleCups - requiredCups
+
+    println(when {
+        difference > 0 -> "Yes, I can make that amount of coffee (and even $difference more than that)"
+        difference < 0 -> "No, I can make only $possibleCups cups of coffee"
+        else -> "Yes, I can make that amount of coffee"
+    })
 }
